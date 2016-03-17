f653ef25dc15693ed8e5a3a5e44edc5a34589061
twtrubiks <twtrubiks@gmail.com>
2016-03-17 20:26:03 +0800

    Improve performance

--- eynyCrawlerMega.py
+++ eynyCrawlerMega.py
@@ -7,8 +7,8 @@ import re
 requests.packages.urllib3.disable_warnings()
 
 
-targetURL='http://www.eyny.com/forum-205-1.html'	
-fileName='eyny-Movie-Mage.txt'
+targetURL = 'http://www.eyny.com/forum-205-1.html'	
+fileName = 'eyny-Movie-Mage.txt'
 
 
 def patternMega(text):
@@ -18,51 +18,50 @@ def patternMega(text):
           return True
 			
 if __name__ == "__main__":
-    Page=10
+    #變數 "Page" 為要對網頁爬的頁數，預設為10頁
+    Page = 10
     print 'Start parsing eyny movie....'
-    rs=requests.session()  
-    res=rs.get(targetURL,verify=False)
-    soup=BeautifulSoup(res.text,'html.parser')
-    pageURL=[]
-    pagelinkALL=[]
+    rs = requests.session()  
+    res = rs.get(targetURL,verify=False)
+    soup = BeautifulSoup(res.text,'html.parser')
+    pageURL, pagelinkALL = [] , []
 
-    pagelinkALL=soup.select('.pg')[0].find_all('a')
-    pageURL.append(targetURL)#first page
-
-    for num in range(1,Page,1):
-        #print pagelinkALL[num].text
-        #print pagelinkALL[num]['href']
-        pageURL.append('http://www.eyny.com/'+pagelinkALL[num]['href'])
+    #first page
+    pageURL.append(targetURL)
+    pagelinkALL = soup.select('.pg')[0].find_all('a')
+    #得到每頁的page link
+    for index in range(1,Page,1):
+        #print pagelinkALL[index].text //title
+        #print pagelinkALL[index]['href'] //href       
+        pageURL.append('http://www.eyny.com/'+pagelinkALL[index]['href'])
 	
-   
-    res=rs.get(targetURL,verify=False)
-    soup=BeautifulSoup(res.text,'html.parser')  
-    #print  soup.select('.common .xst')[5].text
-    #print  soup.select('.common .xst')[5]['href']
-
-    f=open(fileName,'wb')
-    obj=''
-    total=len(pageURL)
-    count=0
-    for URL in pageURL:
-        count+=1       
-        res=rs.get(URL,verify=False)
-        soup=BeautifulSoup(res.text,'html.parser')   
+ 
+    content = ''
+    totalpage = len(pageURL)
+    count = 0
+    #得到每篇包含 mega 的文章
+    for URL in pageURL:       
+        res = rs.get(URL,verify=False)
+        soup = BeautifulSoup(res.text,'html.parser')   
         for titleURL in soup.select('.bm_c tbody .xst'):
-            if(patternMega(titleURL.text)):
-               #print titleURL['href']
-               #print titleURL.text
-               name_write=titleURL.text
-               URL_write='http://www.eyny.com/'+titleURL['href']
-               obj+=name_write+'\n'+URL_write+'\n\n'
-        print "Crawler: " + str(100 * count / total ) + " %."
-        obj+=u'----next page-----\n\n'
+            if ( patternMega(titleURL.text) ):
+               name_write = titleURL.text
+               URL_write = 'http://www.eyny.com/'+titleURL['href']
+               content += name_write+'\n'+URL_write+'\n\n'
+        count+=1
+        print "Crawler: " + str(100 * count / totalpage ) + " %."
+        content += u'----next page-----\n\n'
 
-    f.write(obj.encode('utf-8'))
-    f.close()
-
-   
+    with open(fileName,'wb') as f:
+          f.write( content.encode('utf8') )   
+  
     print '----------END----------'
-    path= os.path.abspath(fileName)
-    #os.system('gedit '+path) #LINUX
-    os.system(path)
+    path = os.path.abspath(fileName)
+
+    os.system('gedit '+path) #LINUX
+    #os.system(path) #Windows
+
+
+
+
+
